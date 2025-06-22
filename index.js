98349c9684be1d925baf2c9cd1846466c70b09e9
EsraaHlila <hlilaesraa@gmail.com>
2025-06-22 15:03:47 +0100

    professionals(admin,doctors,nurses) part

--- index.js
+++ index.js
@@ -119,6 +119,188 @@ app.get('/api/private', authenticateToken, authorizeRoles('doctor', 'nurse'), (r
   res.send('This is a protected route for doctors or nurses');
 });
 
+//route to get all users for admin only
+app.get('/users', authenticateToken, authorizeRoles('admin'), async (req, res) => {
+  const users = await pool.query('SELECT id, name, email, role FROM users');
+  res.json(users.rows);
+});
+
+//get a user by id(admin only)
+app.get('/users/:id', authenticateToken, authorizeRoles('admin'), async (req, res) => {
+  const { id } = req.params;
+  const user = await pool.query('SELECT id, name, email, role FROM users WHERE id = $1', [id]);
+  if (user.rows.length === 0) return res.status(404).send('User not found');
+  res.json(user.rows[0]);
+});
+
+//get your profile
+app.get('/me', authenticateToken, async (req, res) => {
+  const result = await pool.query('SELECT id, name, email, role FROM users WHERE id = $1', [req.user.id]);
+  res.json(result.rows[0]);
+});
+
+//update your profile
+app.put('/me/update', authenticateToken, async (req, res) => {
+  const { name, email, role } = req.body;
+  await pool.query(
+    `UPDATE users SET name=$1, email=$2, role=$3
+     WHERE id = $4`,
+    [name, email, role, req.user.id]
+  );
+  res.send('Profile updated successfully');
+});
+
+
+//update a user (admin only)
+app.put('/users/:id', authenticateToken, authorizeRoles('admin'), async (req, res) => {
+  const { name, email, role } = req.body;
+  const { id } = req.params;
+  await pool.query('UPDATE users SET name = $1, email = $2, role = $3 WHERE id = $4', [name, email, role, id]);
+  res.send('User updated');
+});
+
+
+//admins only(doctors, admin and nurses) can add a new service type
+app.post('/service-types', authenticateToken, authorizeRoles('admin', 'doctor', 'nurse'), async (req, res) => {
+  const { name, description } = req.body;
+
+  try {
+    const result = await pool.query(
+      'INSERT INTO service_types (name, description) VALUES ($1, $2) RETURNING *',
+      [name, description]
+    );
+
+    res.status(201).json({
+      message: 'Service type added successfully',
+      serviceType: result.rows[0]
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to add service type');
+  }
+});
+
+//get the list of all service types
+app.get('/service_types', authenticateToken, authorizeRoles('admin', 'nurse', 'doctor'), async (req, res) => {
+  const result = await pool.query('SELECT * FROM service_types');
+  res.json(result.rows);
+});
+
+
+
+//request or create a service(patients only)
+app.post('/services/request', authenticateToken, authorizeRoles('patient'), async (req, res) => {
+  const { service_type_id, zone, preferred_time } = req.body;
+  try {
+    await pool.query(
+      'INSERT INTO services (patient_id, service_type_id, zone, preferred_time, status) VALUES ($1, $2, $3, $4, $5)',
+      [req.user.id, service_type_id, zone, preferred_time, 'pending']
+    );
+    res.status(201).send('Service requested successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to request service');
+  }
+});
+
+
+
+//assign someone to a service(admins only, doctors ,nurses)
+app.patch('/services/:id/assign', authenticateToken, authorizeRoles('admin', 'nurse', 'doctor'), async (req, res) => {
+  const { id } = req.params;
+  const { assigned_to } = req.body;
+
+  await pool.query('UPDATE services SET assigned_to = $1, status = $2 WHERE id = $3', [assigned_to, 'assigned', id]);
+
+  res.send('Service assigned successfully');
+});
+
+
+//get the list of service requests(admins only: admin, doctor and nurses)
+app.get('/services', authenticateToken, authorizeRoles('admin', 'nurse', 'doctor'), async (req, res) => {
+  const result = await pool.query('SELECT * FROM services');
+  res.json(result.rows);
+});
+
+//update a service status
+app.patch('/services/:id/status', authenticateToken, authorizeRoles('nurse', 'doctor', 'admin'), async (req, res) => {
+  const { id } = req.params;
+  const { status } = req.body;
+
+  try {
+    await pool.query('UPDATE services SET status = $1 WHERE id = $2', [status, id]);
+    res.send(`Service status updated to ${status}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to update status');
+  }
+});
+
+//availability management
+
+// manage and modify availability for nurses and doctors
+app.post('/availability', authenticateToken, authorizeRoles('nurse', 'doctor'), async (req, res) => {
+  const { date, time_slot } = req.body;
+  const user_id = req.user.id;
+
+  await pool.query(
+    'INSERT INTO availability (user_id, date, time_slot) VALUES ($1, $2, $3)',
+    [user_id, date, time_slot]
+  );
+
+  res.send('Availability added');
+});
+
+// see the availability of someone (for all) it has to be just logged in
+app.get('/availability/:user_id', authenticateToken, async (req, res) => {
+  const { user_id } = req.params;
+
+  const result = await pool.query('SELECT * FROM availability WHERE user_id = $1', [user_id]);
+  res.json(result.rows);
+});
+
+
+
+//services history:
+//admin can view all services requests history
+app.get('/services', authenticateToken, authorizeRoles('admin'), async (req, res) => {
+  const result = await pool.query('SELECT * FROM services');
+  res.json(result.rows);
+});
+
+//doctors can view services history assigned to them
+app.get('/services/doctor', authenticateToken, authorizeRoles('doctor'), async (req, res) => {
+    const result = await pool.query('SELECT * FROM services WHERE assigned_to = $1', [req.user.id]);
+    res.json(result.rows);
+  });
+
+//nurses can view services history assigned to them
+app.get('/services/nurse', authenticateToken, authorizeRoles('nurse'), async (req, res) => {
+  const result = await pool.query('SELECT * FROM services WHERE assigned_to = $1', [req.user.id]);
+  res.json(result.rows);
+});
+
+//patients can view their own services requests history
+app.get('/services/history', authenticateToken, authorizeRoles('patient'), async (req, res) => {
+  const result = await pool.query('SELECT * FROM services WHERE patient_id = $1', [req.user.id]);
+  res.json(result.rows);
+});
+
+
+//services status follow up can be viewed by admins only(doctors, nurses, admin)
+app.patch('/services/:id/status', authenticateToken, authorizeRoles('admin', 'doctor', 'nurse'), async (req, res) => {
+  const { status } = req.body;
+  const { id } = req.params;
+  try {
+    await pool.query('UPDATE services SET status = $1 WHERE id = $2', [status, id]);
+    res.send('Service status updated');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to update service');
+  }
+});
+
+
 
 // Start the server
 app.listen(port, () => {
--- medi.sql
+++ medi.sql
@@ -8,11 +8,35 @@ CREATE TABLE users (
   available boolean default false
 );
 
--- Service Requests
-CREATE TABLE service_requests (
+-- Service
+CREATE TABLE services (
   id SERIAL PRIMARY KEY,
-  patient_id INTEGER REFERENCES users(id),
-  service_type VARCHAR(50),
-  status VARCHAR(50) DEFAULT 'pending',
-  scheduled_at TIMESTAMP
+  patient_id INT REFERENCES users(id),
+  type VARCHAR(50),
+  zone VARCHAR(100),
+  schedule TIMESTAMP,
+  assigned_to INT REFERENCES users(id),
+  status VARCHAR(20) DEFAULT 'pending'
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE availability (
+  id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users(id),
+  date DATE,
+  time_slot VARCHAR(20)
+);
+
+CREATE TABLE service_types (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  description TEXT
+);
+
+--// get service history
+--  app.get('/services/history/:user_id', authenticateToken, async (req, res) => {
+--    const { user_id } = req.params;
+--
+--    const result = await pool.query('SELECT * FROM services WHERE patient_id = $1 ORDER BY schedule DESC', [user_id]);
+--   res.json(result.rows);
+--  });
